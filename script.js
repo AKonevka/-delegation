435db1c13929d332dde989544a57c76ec4e1090c
AKonevka <122662320+AKonevka@users.noreply.github.com>
2024-03-01 16:44:47 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,24 @@
-// метод event.stopPropagation() - препятствует продвижению события дальше 
-// но на текущем элементе все обработчики обрабатывают
-
-// метод - event.stopImmediatePropagation(); - предотвращает всплытие и останавиливает обработку
-// событий на текущем элементе 
-
-let elem1 = document.querySelector('#elem1');
-let elem2 = document.querySelector('#elem2');
-let elem3 = document.querySelector('#elem3');
-
-elem1.addEventListener('click', () => {
-    console.log('Зелёный погружение');
-}, true);
-elem1.addEventListener('click', () => {
-    console.log('Зелёный всплытие');
-}, false);
-
-
-elem2.addEventListener('click', (event) => {
-    console.log('Синий погружения');
-}, true);
-
-elem2.addEventListener('click', (event) => {
-    console.log('Синий всплытие');
-    event.stopImmediatePropagation();
-}, false);
-
-elem3.addEventListener('click', () => {
-    console.log('Розовый погружение');
-}, true);
-
-elem3.addEventListener('click', () => {
-    console.log('Розовый всплытие');
-}, false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let parent = document.querySelector('#parent');
-let button = document.querySelector('button');
 let block = document.querySelector('#block');
+let element = document.querySelectorAll('.element');
+let link = document.querySelectorAll('.element__link');
 
-button.addEventListener('click', (event) => {
-    block.classList.add('active');
-    event.stopPropagation();
-});
+block.addEventListener('click', (e) => {
+    let currentTarget = e.target;
+    console.log(currentTarget);
+    for (let item of element) {
+        item.classList.remove('active');
+    }
+    currentTarget.classList.add('active');
+})
 
-parent.addEventListener('click', () => {
-    block.classList.remove('active');
-});
+// element.parentElement - родительский элемент.
+// element.getAttribute('href') - получить значение указанного атрибута
+for (let item of link) {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(item.getAttribute('href'));
+        item.parentElement.style.backgroundColor = 'green';
+        item.parentElement.append(item.getAttribute('href'));
 
-
-
-
-
-
-
+    })
+}
